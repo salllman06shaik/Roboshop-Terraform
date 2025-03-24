db7094989751be3fe7df76766c74d9f9a379f311
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-25 00:16:06 +0530

    user

--- frontend.tf
+++ frontend.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
-
+  vpc_security_group_ids = [sg-0eba58b37f791d36a]
   tags = {
     Name = "frontend"
   }
