ceb555139a5e471b774a486d03985f374ac0a900
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-25 08:49:12 +0530

    user

--- catalogue.tf
+++ catalogue.tf
@@ -1,11 +1,10 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0eba58b37f791d36a"]
   tags = {
-    Name = "frontend"
+    Name = "catalogue"
   }
-
   provisioner "remote-exec" {
 
     connection {
@@ -17,17 +16,17 @@ resource "aws_instance" "frontend" {
 
     inline = [
       "pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/salllmanshaik/Roboshop-Ansible.git roboshop.yml -e component_name=frontend -e env=dev",
+      "ansible-pull -i localhost, -U https://github.com/salllmanshaik/Roboshop-Ansible.git roboshop.yml -e component_name=catalogue -e env=dev",
     ]
   }
 }
 
 
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "mongodb" {
   zone_id = "Z04349021DA1RQVNS1OQS"
-  name    = "frontend-dev"
+  name    = "catalogue-dev"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.catalogue.private_ip]
 }
