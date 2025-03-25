70e3304dcfed907622c0225d318b635e27b4a9a4
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-25 07:27:31 +0530

    user

--- mongodb.tf
+++ mongodb.tf
@@ -1,16 +1,16 @@
-resource "aws_instance" "frontend" {
+resource "aws_instance" "mongodb" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0eba58b37f791d36a"]
   tags = {
-    Name = "frontend"
+    Name = "mongodb"
   }
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "mongodb" {
   zone_id = Z04349021DA1RQVNS1OQS
-  name    = "frontend-dev"
+  name    = "mongodb-dev"
   type    = "A"
   ttl     = 10
-  records = [aws_instance.frontend.private_ip]
+  records = [aws_instance.mongodb.private_ip]
 }
