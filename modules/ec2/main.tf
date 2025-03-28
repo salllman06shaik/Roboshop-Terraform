140367c03ce9d1d000b5150521e2f35b029dabd8
salllmanshaik <hello.salmanshaik@gmail.com>
2025-03-28 08:36:33 +0530

    user

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -27,7 +27,7 @@ resource "null_resource" "frontend" {
     }
 
     inline = [
-      "sudo pip3.11 install ansible",
+      "sudo pip3.11 install ansible hvac",
       "ansible-pull -i localhost, -U https://github.com/salllmanshaik/Roboshop-Ansible.git roboshop.yml -e component_name=${var.name} -e env=${var.env} -e vault_token={var.vault_token}",
     ]
   }
