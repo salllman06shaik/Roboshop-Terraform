9ce80c9364abec4ee769cd79d8595a34168d801c
salllmanshaik <hello.salmanshaik@gmail.com>
2025-04-05 16:01:22 +0530

    docker

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@
 
 module "eks" {
   for_each = var.eks
-  source = "./module/eks"
+  source = "./modules/eks"
   env = var.env
   eks_version = each.value["eks_version"]
   subnets = each.value["subnets"]
