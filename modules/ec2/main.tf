dca939e0113afa3e842bf88ad56cab40b7994c98
salllmanshaik <hello.salmanshaik@gmail.com>
2025-05-13 18:32:51 +0530

    docker

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -20,7 +20,7 @@ resource "aws_route53_record" "record" {
   records = [aws_instance.instance.private_ip]
 }
 
-resource "null_resource" "frontend" {
+resource "null_resource" "instance_dns" {
   depends_on = [aws_route53_record.record]
 
   triggers = {
