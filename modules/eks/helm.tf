0010b4d8375c0e967b304029568b4108afa511e3
salllmanshaik <hello.salmanshaik@gmail.com>
2025-05-15 16:14:57 +0530

    docker

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -154,7 +154,10 @@ resource "null_resource" "external-secret-store" {
   depends_on = [null_resource.kubeconfig, helm_release.external-secrets]
   provisioner "local-exec" {
     command = <<EOF
-kubectl wait --for=condition=ready pod -l app.kubernetes.io/name=external-secrets -n kube-system --timeout=180s
+until kubectl get crd clustersecretstores.external-secrets.io >/dev/null 2>&1; do
+  echo "Waiting for CRD clustersecretstores.external-secrets.io..."
+  sleep 5
+done
 kubectl apply -f - <<EOK
 apiVersion: v1
 kind: Secret
