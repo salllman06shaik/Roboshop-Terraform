46bc158ca4e909a4e23b7572a06d5d315632b396
salllmanshaik <hello.salmanshaik@gmail.com>
2025-06-03 00:18:18 +0530

    docker

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,6 +16,7 @@ resource "aws_subnet" "main" {
   for_each   = var.subnets
   vpc_id     = aws_vpc.main.id
   cidr_block = each.value["cidr"]
+  availability_zone = each.value["zone"]
 
   tags = {
     Name = each.key
